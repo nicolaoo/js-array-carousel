875c43840fcb7cc7596a793c827694f34eba0639
nicola00 <nicolajobs00@gmail.com>
2023-02-06 15:35:57 +0100

    FEAT js take element from html

---
+++ app.js
@@ -0,0 +1,8 @@
+// dichiaro le variabili delle immagini e delle frecce
+
+const pictureEl = document.getElementsByClassName('picture')
+const arrowLeft = document.querySelector('.arrow-left')
+const arrowRight = document.querySelector('.arrow-right')
+
+console.log(pictureEl, arrowLeft,arrowRight)
+
