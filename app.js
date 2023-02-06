0d621947a5f29eab6c6c53f92f9188421453e00b
nicola00 <nicolajobs00@gmail.com>
2023-02-06 16:48:30 +0100

    FEAT arrow left

--- app.js
+++ app.js
@@ -4,5 +4,40 @@ const pictureEl = document.getElementsByClassName('picture')
 const arrowLeft = document.querySelector('.arrow-left')
 const arrowRight = document.querySelector('.arrow-right')
 
-console.log(pictureEl, arrowLeft,arrowRight)
+let pictureAddRight  = 0
+let pictureAddLeft = 5
 
+let listPicture = [
+    './Img/01.jpg',
+    './Img/02.jpg',
+    './Img/03.jpg',
+    './Img/04.jpg',
+    './Img/05.jpg',
+]
+
+console.log(listPicture)
+
+arrowRight.addEventListener('click', function () {
+
+    let pictureBefore = pictureEl[pictureAddRight]
+    pictureBefore.classList.remove('active')
+
+    pictureAddRight += 1
+    
+    let pictureAfter = pictureEl[pictureAddRight]
+    pictureAfter.classList.add('active')
+    
+})
+
+
+arrowLeft.addEventListener('click', function () {
+    
+    let pictureBefore = pictureEl[pictureAddRight]
+    pictureBefore.classList.remove('active')
+
+    pictureAddRight += 1
+    
+    let pictureAfter = pictureEl[pictureAddRight]
+    pictureAfter.classList.add('active')
+
+})
